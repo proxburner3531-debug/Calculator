f11f4b6ff1878ba67cc510e72065ad9ed2d6735d
Abhijeet Pandey <proxburner3531@gmail.com>
2026-07-03 15:43:37 +0530

    Implemented multiplication function

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -10,12 +10,17 @@ float Subtraction(float a,float b)
 	return a - b;
 }
 
+float Multiplication(float a, float b)
+{
+	return a * b;
+}
+
 int main()
 {
 	float a, b, res;
 	std::cin >> a >> b;
 
-	res = Subtraction(a, b);
+	res = Multiplication(a, b);
 
 	std::cout << res;
 
