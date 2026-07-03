b2dbd32f6c76d1dfbd33a899c7de46b7cd9823e2
Abhijeet Pandey <proxburner3531@gmail.com>
2026-07-03 15:41:55 +0530

    Implemented Subtraction function

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -5,12 +5,17 @@ float Addition(float a, float b)
 	return a + b;
 }
 
+float Subtraction(float a,float b)
+{
+	return a - b;
+}
+
 int main()
 {
 	float a, b, res;
 	std::cin >> a >> b;
 
-	res = Addition(a, b);
+	res = Subtraction(a, b);
 
 	std::cout << res;
 
